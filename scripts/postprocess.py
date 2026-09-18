4928b0fe3fb49d67b7b242e224c13314f1b04e55
deepin <10000@tiyicn.cn>
2026-09-18 13:11:17 +0800

    初始化

--- scripts/postprocess.py
+++ scripts/postprocess.py
@@ -51,7 +51,7 @@ def fix_bold(text):
             return line + '**'
         return line[:idx] + line[idx + 2:]
     text = '\n'.join(_fix_bold_per_line(ln) for ln in text.split('\n'))
-    text = re.sub(r'\*\* (?=\S)', '**', text)
+    text = re.sub(r'\*\*(.+?)\*\*', _strip_bold, text)
     return text
 
 
